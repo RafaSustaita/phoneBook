82ec54a0f0ba8b6fe1f0d6b560e739833c1ee8b7
Rafael <rafaelsustaita@gmail.com>
2023-08-17 21:45:06 -0300

    Prueba

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
-
-
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -111,7 +109,7 @@ app.put('/api/persons/:id', (request, response) => {
 });
 
 
-const PORT = process.env.PORT ?? 3001;
+const PORT = 'http://localhost:3001/api/persons'
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
